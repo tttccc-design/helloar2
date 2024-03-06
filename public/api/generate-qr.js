7ef9291d666b7a70ac10092942c903b3721add49
tttccc-design <38571897+tttccc-design@users.noreply.github.com>
2024-03-05 21:57:56 -0500

    Update generate-qr.js

--- public/api/generate-qr.js
+++ public/api/generate-qr.js
@@ -3,7 +3,8 @@ const QRCode = require('qrcode');
 module.exports = async (req, res) => {
     const { data } = req.body;
     try {
-        const qr = await QRCode.toDataURL(data);
+        const qr = await QRCode.toDataURL(curl https://helloar2.vercel.app/api/generate-qr
+);
         res.status(200).send({ qr });
     } catch (error) {
         res.status(500).send({ error: 'Failed to generate QR code' });
